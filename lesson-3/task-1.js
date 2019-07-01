83c267e84a6c747a550520784f9018c2a982b038
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-07-01 19:13:15 +0300

    fix_2

--- lesson-3/task-1.js
+++ lesson-3/task-1.js
@@ -14,13 +14,22 @@ let date = new Date();
 console.log(date.getMonth());
 console.log(date.getDate());
 
-if (date.getMonth() == 0 || date.getMonth() == 2 || date.getMonth() == 4 || date.getMonth() == 6 || date.getMonth() == 7 || date.getMonth() == 9 || date.getMonth() == 11) {
+if (date.getMonth() == 0 || 
+	date.getMonth() == 2 || 
+	date.getMonth() == 4 || 
+	date.getMonth() == 6 || 
+	date.getMonth() == 7 || 
+	date.getMonth() == 9 || 
+	date.getMonth() == 11) {
 	if (date.getDate() <= 11) {
 		person.salary = console.log(`good salary`);
 	} else {
 		person.salary = console.log(`bad salary`);
 	}
-} else if(date.getMonth() == 3 || date.getMonth() == 5 || date.getMonth() == 8 || date.getMonth() == 10) {
+} else if(date.getMonth() == 3 || 
+	date.getMonth() == 5 || 
+	date.getMonth() == 8 || 
+	date.getMonth() == 10) {
 	if (date.getDate() <= 10) {
 		person.salary = console.log(`good salary`);
 	} else {
