e71d939bef7081f2f167a86f66c121b92f1fe90d
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-07-01 19:11:49 +0300

    fix_1

--- lesson-3/task-1.js
+++ lesson-3/task-1.js
@@ -8,15 +8,31 @@
 
 const person = {};
 
-let date = new Date();
-console.log(date.getDate());
-if (date.getDate() <= 10) {
-	person.salary = console.log(`good salary`);
-} else {
-	person.salary = console.log(`bad salary`);
-}
-
 // Решение
+
+let date = new Date();
+console.log(date.getMonth());
+console.log(date.getDate());
+
+if (date.getMonth() == 0 || date.getMonth() == 2 || date.getMonth() == 4 || date.getMonth() == 6 || date.getMonth() == 7 || date.getMonth() == 9 || date.getMonth() == 11) {
+	if (date.getDate() <= 11) {
+		person.salary = console.log(`good salary`);
+	} else {
+		person.salary = console.log(`bad salary`);
+	}
+} else if(date.getMonth() == 3 || date.getMonth() == 5 || date.getMonth() == 8 || date.getMonth() == 10) {
+	if (date.getDate() <= 10) {
+		person.salary = console.log(`good salary`);
+	} else {
+		person.salary = console.log(`bad salary`);
+	}
+} else if(date.getMonth() == 1) {
+	if (date.getDate() <= 8) {
+		person.salary = console.log(`good salary`);
+	} else {
+		person.salary = console.log(`bad salary`);
+	}
+};
 
 person.salary; // good salary
 
