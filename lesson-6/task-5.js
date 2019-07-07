7ce749b9b44c4999d83135bbaf4f1acc52edc4b5
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-07-07 18:07:37 +0300

    add task-5

--- lesson-6/task-5.js
+++ lesson-6/task-5.js
@@ -17,6 +17,25 @@ const INITIAL_ACCUMULATOR = 6;
 
 // Решение
 
+const required = () => {throw new Error('required parameter')};
+
+const reduce = (array = required(), callback = required(), number = required()) => {
+	if (Array.isArray(array) && typeof(callback) === 'function' && typeof(number) === 'number') {
+		let result = number;
+ 	for (let index = 0; index < array.length; index++) {
+    if (callback(array[index], index, array)) {
+    	result = callback(result, array[index], index, array);
+    };
+  } return result;
+	} else if (!Array.isArray(array)) {
+		throw new Error ('first argument must be an array');
+	} else if (typeof(callback) !== 'function') {
+		throw new Error ('second argument must be a function');
+	} else if (typeof(number) !== 'number') {
+		throw new Error ('third argument must be a number');
+	};
+};
+
 const result = reduce(
     array,
     function(accumulator, item, i, arrayRef) {
